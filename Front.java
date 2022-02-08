2005449f807468f6b57b344080ab6b8ce7a8aea6
Abhishek Jadhav <99069318+Abhijadhav5001@users.noreply.github.com>
2022-02-08 21:37:33 +0530

    Front.java

--- Front.java
+++ Front.java
@@ -1,170 +1,311 @@
+/*
+Writting a appkicationn to design the front end for the File packer Unpacker Project
+*/
 import java.lang.*;
+import java.util.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
-import java.util.*;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
 
 class Front
 {
-	public static void main(String arg[])
-	{
-		Window obj = new Window();
-	}
-}
+    public static void main(String arg[])
+    {
+        //call the Window class object
+        Window wobj = new Window();
 
-class Window //implements ActionListener
+    }//End of main function
+
+}//End of class Front
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//class : Window
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Window
 {
-	public Window()
-	{
-		JFrame f = new JFrame("Marvellous Login");
-		
-		JButton bobj = new JButton("Submit");
-		bobj.setBounds(100,200,140,40);
-		// --------------------
-		JLabel lobj1 = new JLabel("Enter Folder name");
-		lobj1.setBounds(10,10,100,100);
+    
 
-		JTextField tf1 = new JTextField();
-		// (x cordinate, y cordinate, widtyh, height)
-		tf1.setBounds(100,50,130,30);
-		// ------------------------------------
-		JLabel lobj2 = new JLabel("Enter File name");
-		lobj2.setBounds(10,110,100,100);
+    public Window()         //default constructor of class Window
+    {
+        //jyachi survat J pasun hotey e.g JFrame is part of swing
+        JFrame f = new JFrame(" Marvellous Login ");
 
-		JTextField tf2 = new JTextField();
-		// (x cordinate, y cordinate, widtyh, height)
-		tf2.setBounds(100,120,130,30);
+        JButton bobj = new JButton(" Submit ");
+        //bobj.setBounds(130,100,100, 40 );
+        bobj.setBounds(150, 150, 140, 40);
+        
+        // JButton bobj2 = new JButton("Submit");
+        // bobj2.setBounds(150, 190, 140, 40);
 
-		f.add(lobj1);
-		f.add(bobj);
-		f.add(tf1);
-		f.add(lobj2);
-		f.add(tf2);
+        JLabel lobj = new JLabel("File Name : ");
+        //lobj.setBounds(50,50, 100,30);
+        lobj.setBounds(10, 10, 100, 100);
 
-		f.setSize(300,300);
-		f.setLayout(null);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTextField tf = new JTextField();
+        //tf.setBounds(50,100, 200,30);
+        tf.setBounds(110, 50, 130, 30);
 
-		bobj.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent eobj){
-				Packer pobj = new Packer(tf1.getText(), tf2.getText());
-				f.setVisible(false);
-				NewWindow o = new NewWindow();
-			}
-		});
-	}
+        JLabel lobj2 = new JLabel("Folder Name : ");
+        lobj2.setBounds(10, 60, 100, 100 );
+
+        JTextField tf2 = new JTextField();
+        tf2.setBounds(110, 90, 130, 30);
+
+        f.add(lobj);
+        f.add(bobj);
+        f.add(lobj2);
+       // f.add(bobj2);
+        f.add(tf);
+        f.add(tf2);
+
+        f.setSize(400, 400);                        //setSize(width,height). setSize() is a predfined method of JFrame class
+        f.setLayout(null);                          //method of JFrame class
+        f.setVisible(true);                         //method of JFrame class
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //int EXIT_ON_CLOSE = 3.  Terminal closes when the JFrame is closed . Standard  way to do it . Application window is created and closed also
+                                                                //method of JFrame class
+
+        bobj.addActionListener( new ActionListener()                //Register the component with List //component.addActionListner(instance of  Listener Class)
+        {
+            public void actionPerformed(ActionEvent eobj)         //anonymous function,  Override the actionPerformed() method
+            {
+              System.out.println("Button Clicked");
+              System.out.println("Folder Name : "+ tf2.getText());
+
+              System.out.println("File Entered : " + tf.getText());   //retrive the data entered in the TextField when the button is clicked
+
+              PackerX pobj = new PackerX( tf.getText(), tf2.getText() );    //PackerX class object created and it is called implicly when its constructor is created
+                                        //FileName    , FolderName
+             f.setVisible(false);
+             
+             NewWindow newObj = new NewWindow();
+
+            }
+        }
+        );
+    }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//class : NewWindow
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class NewWindow
 {
-	public NewWindow()
-	{
-		JFrame fobj = new JFrame("New frame");
-		fobj.setSize(300,300);
-		fobj.setVisible(true);
-		fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    public NewWindow()
+    {
+        JFrame fobj2 = new JFrame("New Window");                //new window will be opened when FileName and FolderName is passed and submit buttin is clicked in first window
+        
+        JLabel Lobj = new JLabel("Packed File: ");              //creating a object of the class JLabel
+        Lobj.setBounds(10,10, 50,50);                           //setBounds(x, y, width, height)
+        
+        JTextField TFobj = new JTextField();
+        TFobj.setBounds(70, 20, 110, 30);
+
+        JButton Bobj = new JButton("Submit");                   //Text field
+        Bobj.setBounds(60, 80, 110, 30);
+
+        fobj2.add(Lobj);                                        //adding lable to the new window
+        fobj2.add(Bobj);
+        fobj2.add(TFobj);
+
+        fobj2.setSize(350, 350);                                //setSize(width, height);
+        fobj2.setLayout(null);
+        fobj2.setVisible(true);
+        
+        fobj2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Bobj.addActionListener( new ActionListener()
+        {
+            public void actionPerformed(ActionEvent eobj2)
+            {
+                
+                System.out.println("Button on New Window clicked ");
+                System.out.println("Packed File name entered is : "+ TFobj.getText() );
+                UnpackerX unObj = new UnpackerX( TFobj.getText() );
+            }
+        }
+         );
+
+    }
 }
 
-class Packer
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Class : PAckerX
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class PackerX
 {
-	// Object for file writing
-	public FileOutputStream outstream = null;
+    public FileOutputStream outstream;
+    
+    public PackerX(String FileName, String DirName)
+    {
+        System.out.println("Inside class Packer");
+        try
+        {
+            File outfile = new File(FileName);
 
-	// parametrised constructor
-	public Packer(String FolderName, String FileName)
-	{
-		try
-		{
-			// Create new file for packing
-			File outfile = new File(FileName);
-			outstream = new FileOutputStream(FileName);
+            outstream = new FileOutputStream(FileName);
 
-			// Set the current working directory for folder traversal
-			// System.setProperty("user.dir",FolderName);
-			
-			TravelDirectory(FolderName);
-		}
-		catch(Exception obj)
-		{
-			System.out.println(obj);
-		}
-	}
+        }
+        catch(Exception obj)
+        {
+            System.out.println(obj);
+        }
+        
+        //call TravelDirectory Function
+        TravelDirectoryX(DirName);
 
-	public void TravelDirectory(String path)
-	{
-		File directoryPath = new File(path);
-		int counter = 0;
-		// Get all file names from directory
-		File arr[] = directoryPath.listFiles();
+    }//End of PackerX class Constructor
 
-		System.out.println("-------------------------------");
-		for(File filename : arr)
-		{
-			//System.out.println(filename.getAbsolutePath());
-			
-			if(filename.getName().endsWith(".txt"))
-			{
-				counter++;
-				System.out.println("Packed file : "+filename.getName());
-				PackFile(filename.getAbsolutePath());	
-			}		
-		}
-		System.out.println("-------------------------------");
-		System.out.println("Succesfully packed files : "+counter);
-		System.out.println("-------------------------------");
-	}
+    public void TravelDirectoryX(String DirName)
+    {
+        File dirObj = new File(DirName);
 
-	public void PackFile(String FilePath)
-	{
-//		System.out.println("File name received "+ FilePath);
-		byte Header[] = new byte[100];
-		byte Buffer[] = new byte[1024];
-		int length = 0;
+        File arr[] = dirObj.listFiles();
 
-		FileInputStream istream = null;
+        for(File filename : arr)
+        {
+            
+            if(filename.getName().endsWith(".txt") )
+            {
+                
+                System.out.println(filename.getName());
+                
+                //call PackFile() function
+                PackFile( filename.getAbsolutePath() );
 
-		File fobj = new File(FilePath);
+            }
+        
+        }
 
-		String temp = FilePath+" "+fobj.length();
-		
-			// Create header of 100 bytes
-		for(int i = temp.length(); i< 100; i++)
-		{
-			temp = temp + " ";
-		}	
+    }//End of TravelDirectoryX function
 
-		Header = temp.getBytes();
-		try
-		{
-			// open the file for reading
-			istream = new FileInputStream(FilePath);
+    public void PackFile(String FilePath)
+    {
+        byte Header[] = new byte[100];                      //will contain the files Absolutepath and length of the data in the file
+        byte Buffer[] = new byte[1024];                    //
+        
+        int length = 0;
 
-			outstream.write(Header,0,Header.length);
-			
-			while((length = istream.read(Buffer)) > 0)
-			{
-				outstream.write(Buffer,0,length);
-			}
+        FileInputStream instream;
 
-			istream.close();
-		}
-		catch(Exception obj)
-		{}
-		// System.out.println("Header : "+temp.length());
+        File fobj = new File(FilePath);
 
-		// Packing logic
-	}
+        String temp = FilePath+" "+fobj.length();
+        //String temp = FilePath;
+
+        System.out.println("The length of the string when FilePath and files data length is added is : "+temp.length());
+
+        //Adding white spaces to the string temp which will further converted into the 100 bytes Header
+        for(int i = temp.length(); i < 100; i++)
+        {
+            temp = temp + " ";
+        }
+
+        System.out.println("The length of the String ' temp ' when WhiteSpaces is added to it along with FilePath and files data is : "+temp.length());
+
+        Header = temp.getBytes();
+        System.out.println("The length of the Header in bytes when converted from the String is : "+Header.length);
+
+        // for(int j = 0; j < Header.length; j++)
+        // {
+        //     System.out.println(Header[j]);
+        // }
+
+       try
+       {
+            
+
+            instream = new FileInputStream(FilePath);
+
+            outstream.write( Header, 0, Header.length );
+
+            while( ( length = instream.read(Buffer) ) != -1)
+            {
+                outstream.write( Buffer, 0, length );
+            }
+            
+            instream.close();
+            
+       }
+       catch(Exception obj)
+       {
+           System.out.println(obj);
+       }
+       
+    }//End of Funtion PackFile
+
+}//End of class Packer
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//class : unPackerX
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class UnpackerX
+{
+    public UnpackerX(String PackFileName)
+    {
+        unPacking(PackFileName);
+    }
+
+    public void unPacking(String FileSrc)
+    {
+        byte Header[] = new byte[100];
+        int length = 0;
+
+        FileInputStream instream;
+
+        try
+        {
+            instream = new FileInputStream(FileSrc);
+
+            while( ( length = instream.read(Header, 0, 100) ) > 0)
+            {
+                //convert the data writen from file from bytes to string
+                String str = new String(Header);
+
+                //Herer we get files absulute path and lengthof data in the file as a whole string
+                //so find a extension in string form where we would recive only the files name and length of the data in the file
+                
+                String ext = str.substring(str.lastIndexOf("/"));
+                System.out.println("The extension string got is  : " + ext);
+                System.out.println("");
+
+                ext = ext.substring(1);
+                System.out.println("The extension string when / is removed from it is : " + ext);
+                System.out.println("");
+
+                //now we have have name of the file and length of data in the file together as a string.
+                //split this string data into array of string 
+                String arr[] = ext.split("\\s");
+
+                //Now we have in a array name of file at begin index and length of file in the next index
+                //we assign the name of the file to new String 'name' 
+                String name = arr[0];
+
+                //we convert the length from string to integer
+                int size = Integer.parseInt(arr[1]);
+
+                //Now we create new file from the packed File using FileOutputStream class
+                FileOutputStream outstream = new FileOutputStream( name );
+
+                ///now we will read the data from the file in the newly created file
+                byte buffer[] = new byte[size];
+
+                instream.read(buffer);
+                //now this data read from file is to be wriiten in the newly created file
+                outstream.write(buffer, 0, size);
+            }
+            instream.close();
+        }
+        catch(Exception obj)
+        {
+            System.out.println(obj);
+        }
+    }
 }
-
-
-
-
-
-
-
